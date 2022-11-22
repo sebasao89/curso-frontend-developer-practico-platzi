e0421e3b37bc4e55cf4ce1460c8d25317a84b0ef
Sebasao <sebas.ao@outlook.com>
2022-11-21 21:46:41 -0500

    Menus con media query y utilizacion de javascript para activar

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamburguesa = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMenuHarburguesa);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMenuHarburguesa() {
+    mobileMenu.classList.toggle('inactive')
 }
